3e4f89f840afd5de13263a822d10175694030cb8
lutfiangga <lutfiangga2908@gmail.com>
2023-10-10 21:14:42 +0700

    test2

--- src/pages/LaporanSaya.jsx
+++ src/pages/LaporanSaya.jsx
@@ -6,7 +6,8 @@ const LaporanSaya = () => {
 
       <h1>Test</h1>
       <h1>Test Dharma</h1>
-      <p>lorem10</p>
+      <h3>Judul</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque alias minima pariatur, facere nostrum maiores!</p>
     </div>
   );
 };
