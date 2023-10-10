32d2807ce0ff276b4ed0e2d32543cda99528cf12
Inas Afifah <afifahins1510@gmail.vomgit config --global user.email afifahins1510@gmail.com>
2023-10-10 21:39:20 +0700

    test

--- src/pages/LaporanSaya.jsx
+++ src/pages/LaporanSaya.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const LaporanSaya = () => {
   return (
     <div>
+
       <h1>Test</h1>
       <h1>Test Dharma</h1>
-      <p>lorem10</p>
-      <p> tes </p>
+      <h1>testt</h1>
+
     </div>
   );
 };
