dd336304a72a7a826dd49d476edee7d0bdf78a92
lutfiangga <lutfiangga2908@gmail.com>
2023-10-11 18:41:36 +0700

    add landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -18,9 +18,9 @@ const Landing = () => {
       </div>
 
       {/* Welcome Page */}
-      <div className='bg-bg-primary w-screen h-100 py-32 flex px-32'>
+      <div className='bg-bg-primary w-screen h-120 py-32 flex px-32'>
         {/* left side */}
-        <div className="container mx-auto text-white flex justify-center items-start flex-col">
+        <div className="container text-white flex justify-center items-start flex-col">
           <div className='font-bold text-5xl'>Laporkan <br /> Masalahmu</div>
           <div className='text-md'>Sampaikan laporanmu langsung kepada <br /> instansi pemerintah yang berwenang</div>
           <div className='flex flex-row gap-5 items-center'>
@@ -29,9 +29,24 @@ const Landing = () => {
           </div>
         </div>
         {/* right side */}
-        <div className='container mx-auto flex justify-center items-end'>
+        <div className='container flex justify-center items-end'>
           <img src="../img/people.png" alt="people" />
         </div>
+      </div>
+
+      {/* tentang perusahaan */}
+      <div className='w-screen'>
+      <div className='text-center text-blue-600 text-4xl font-bold mt-10'>Tentang Perusahaan</div>
+      <div className='text-center text-md'>Kenali lebih lanjut tentang diperusahaan dan tim dibaliknya</div>
+        <div className='flex flex-row'>
+      <div className='bg-bg-primary w-5/12 h-96 mt-5 text-center text-white py-12 px-24 flex flex-col place-content-center rounded-r-lg'>
+        <div className='font-bold text-4xl'>Cerita Kita</div>
+        <div className='text-sm mt-5 text-justify'>Semuanya dimulai dengan visi bersama untuk menjadikan NTT tempat yang lebih baik untuk tinggal, bekerja, dan bepergian. Pada Tahun 2023, sekelompok individu yang penuh semangat berkumpul untuk mengatasi masalah mendesak mengenai masalah jalan dan infrastruktur di komunitas kita tercinta.</div>
+      </div>
+      <div className='w-5/12 h-96 mt-5 py-12 px-24 place-content-center justify-items-end'>
+        <img src="../img/people.png" alt="Cerita" />
+      </div>
+      </div>
       </div>
     </div>
   );
